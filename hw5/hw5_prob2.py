904de64f64e03c8b7996791535ba3de909a1d038
maria8ch <maria8ch@gmail.com>
2020-03-19 20:38:13 -0700

    finished hw5

--- hw5/hw5_prob2.py
+++ hw5/hw5_prob2.py
@@ -3,34 +3,67 @@ import pycuda.curandom as rand
 import pycuda.driver as cuda
 import pycuda.autoinit
 import time
-import numpy
-
-def circle_time(x_and_y):
-
-    return x_and_y[:,0]**2 + x_and_y[:,1]**2 < 1
+import numpy as np
 
 
-def find_pi_cuda(function, n, x_lim=[-1,1],y_lim=[-1,1]):
-    '''Takes in integer n (n points used for integration), does Monte Carlo to find pi using a mask. Returns int pi'''
+def find_pi_cuda(n):
+    '''Takes in integer n (n points used for integration), does Monte Carlo to
+    find pi using gpu methods. Returns int pi'''
+
+    #  x and y coordinates generated
+    rg = rand.XORWOWRandomNumberGenerator()
+    x = rg.gen_uniform(n, np.float32)
+    y = rg.gen_uniform(n, np.float32)
+
+    # circle times
+    am_circle = x**2 + y**2
+
+    outside = pycuda.gpuarray.zeros_like(x, np.float32)
+    inside = pycuda.gpuarray.ones_like(x, np.float32)
+
 
     # here's Monte Carlo for finding pi
-    x_side = x_lim[1]-x_lim[0]
-    y_side = y_lim[1]-y_lim[0]
-    Volume_boi = x_side*y_side
 
-    # x and y
-    rg = rand.XORWOWRandomNumberGenerator()
-    points = rg.gen_uniform((n,2), np.float32)
-    points[:,0] = points[:,0]*x_side+x_lim[0]
-    points[:,1] = points[:,1]*y_side+y_lim[0]
-    pibois = function(points)
+    H = pycuda.gpuarray.if_positive(am_circle <= 1, inside, outside)
+    pi = 4 * gp.sum(H)/n
 
-    return gp.sum(pibois)/n*V
+    return pi
 
 
 n = 2**8
 start = time.time()
-pi = find_pi_cuda(circle_time,n)
+pi = find_pi_cuda(n)
 stop = time.time()
 
-print("Time to run:", stop - start, "seconds; pi is", pi)
+print("Time to run 2^8 points:", stop - start, "seconds; pi is", pi)
+
+n = 2**16
+start = time.time()
+pi = find_pi_cuda(n)
+stop = time.time()
+
+print("Time to run 2^16 points:", stop - start, "seconds; pi is", pi)
+
+n = 2**24
+start = time.time()
+pi = find_pi_cuda(n)
+stop = time.time()
+
+print("Time to run 2^24 points:", stop - start, "seconds; pi is", pi)
+
+n = 2**29
+start = time.time()
+pi = find_pi_cuda(n)
+stop = time.time()
+
+print("Time to run 2^29 points:", stop - start, "seconds; pi is", pi)
+
+n = 2**30
+start = time.time()
+pi = find_pi_cuda(n)
+stop = time.time()
+
+print("Time to run 2^30 points:", stop - start, "seconds; pi is", pi)
+
+# After some guess and check, 2^29 is the highest it can go without running out
+# memory.
